9edd4d6f432d08391111fecc96df5300eb44d437
khenverlie54-sketch <khenverlie54@gmail.com>
2025-10-05 23:30:48 +0800

    Book.java

---
+++ Book.java
@@ -0,0 +1,14 @@
+public class Book {
+    public String title;
+    public String author;
+    public int yearPublished;
+    public int currentPage;
+
+    public Book(String title, String author, int yearPublished, int currentPage) {
+        
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+        this.currentPage = currentPage;
+    }
+}
